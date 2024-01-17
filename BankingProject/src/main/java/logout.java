84cdfc260aebbbcd63a927ad47f10b1ae6614ed4
vinay bhaskar <139907258+vinay575@users.noreply.github.com>
2024-01-17 15:17:51 +0530

    update

--- BankingProject/src/main/java/logout.java
+++ BankingProject/src/main/java/logout.java
@@ -1,50 +1,29 @@
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
 
 @WebServlet("/Logout")
 public class logout extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
- 
-    public logout() {
-        super();
-      
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("user");
+        
+        response.sendRedirect("Login.html");
     }
 
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		session.removeAttribute("user");
-		
-		String stmtStr = "<html><body style='text-align: center; background-color: #f4f4f4; padding: 20px; border: 2px solid #ddd; border-radius: 10px; height:100px; width:80%; margin: 0 auto; margin-top :10px; '>"
-				+ "<h2  style=\"color: red; font-weight: bold; \">Logout complete. Your financial information is secure. For any inquiries, reach out to our customer support</h2>"
-				+ "<form action='LoginServlet' method='post' style='margin-top: 20px;'>\r\n"
-			    + " <input type='submit' value='Login' style='padding: 10px; background-color: #4CAF50; color: white; border: none; border-radius: 5px; cursor: pointer;'>\r\n"
-			    + "</form> "
-			    + "</body></html>";
-
-		response.getWriter().append(stmtStr);
-		
-	}
-
     
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		doGet(request, response);
-	}
-
+  
+    
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
-
-
-
-
-
-
