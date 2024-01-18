ea6a9e5c5d75d3e2decd43662fbbb408fe8c0d0d
vinay bhaskar <139907258+vinay575@users.noreply.github.com>
2024-01-18 20:28:15 +0530

    Update RegistrationServlet.java

--- BankingProject/src/main/java/RegistrationServlet.java
+++ BankingProject/src/main/java/RegistrationServlet.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
-
+import DAO.BankDAO;
 import DAO.UserDAO;
 import DTO.UserDTO;
 import jakarta.servlet.RequestDispatcher;
